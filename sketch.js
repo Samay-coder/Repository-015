bab850380e5f84e71587f5dd6ac8cb7cde2fbf43
Samay-coder <67533025+Samay-coder@users.noreply.github.com>
2020-08-09 11:04:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -124,6 +124,32 @@ if(backgroundImg){
   image(polygon_img ,ball.position.x,ball.position.y,40,40);
 
   slingShot.display();
+
+  block1.score();
+  block2.score();
+  block3.score();
+  block4.score();
+  block5.score();
+  block6.score();
+  block7.score();
+  block8.score();
+  block9.score();
+  block10.score();
+  block11.score();
+  block12.score();
+  block13.score();
+  block14.score();
+  block15.score();
+  block16.score();
+  blocks1.score();
+  blocks2.score();
+  blocks3.score();
+  blocks4.score();
+  blocks5.score();
+  blocks6.score();
+  blocks7.score();
+  blocks8.score();
+  blocks9.score();
 }
 function mouseDragged(){
   Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
